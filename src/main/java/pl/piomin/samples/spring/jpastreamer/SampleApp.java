fbfdddbfc417f098775eeb5bc90700d38209fa91
piomin <piotr.minkowski@gmail.com>
2023-06-02 09:17:08 +0200

    spring data workaround

--- src/main/java/pl/piomin/samples/spring/jpastreamer/SampleApp.java
+++ src/main/java/pl/piomin/samples/spring/jpastreamer/SampleApp.java
@@ -1,7 +1,11 @@
 package pl.piomin.samples.spring.jpastreamer;
 
+import com.speedment.jpastreamer.application.JPAStreamer;
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SampleApp {
@@ -9,4 +13,12 @@ public class SampleApp {
     public static void main(String[] args) {
         SpringApplication.run(SampleApp.class, args);
     }
+
+    // TODO - it is some kind of workaround since it didn't work
+    // remove it once it works without it (build should fail)
+    @Bean
+    JPAStreamer jpaStreamer(EntityManagerFactory entityManagerFactory) {
+        return JPAStreamer.createJPAStreamerBuilder(entityManagerFactory).build();
+    }
+
 }
